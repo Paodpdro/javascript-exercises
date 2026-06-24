a97f6818ae46582fb2c8f9a8e329b263aa2ffc70
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-06-25 00:40:39 +0300

    Did the Loops and Arrays assignemnt from 06 to 11

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let arr = text.split('');
+    let result = arr.reverse().join('');
 
+    return result
 };
 
 // Do not edit below this line
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    let ogArray =  Array.from(arguments);
+    let first = ogArray[0];
+    let rest = ogArray.slice(1);
+
+    let result = first.filter(item => !rest.includes(item));
+    
+    return result
 };
 
 // Do not edit below this line
