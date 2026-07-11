337a0d3c78bdcd73e25eda7d5272f9439175da2e
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-07-11 22:52:49 +0300

    Finished 14th exercise - fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let n = 1;
+    let m = 1;
 
+    if (num < 0) {
+        return "OOPS";
+    }
+    else if (num ==0) {
+        return 0;
+    }
+    else if (num == 1 || num ==2) {
+        return m;
+    }
+    else {
+        for(let i = 3; i<=num; i++) {
+        result = n + m;
+        n = m;
+        m = result;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
