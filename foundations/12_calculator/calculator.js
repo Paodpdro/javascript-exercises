a83d37f3368945565111f74ffec7f17da7373e2c
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-07-11 21:28:07 +0300

    Finished 12th exercise calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, curr) => acc+curr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr) => acc*curr)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let result = 1
+	for(let i = 0; i < num1; i++) {
+    result *= i+1
+  }
+  return result
 };
 
 // Do not edit below this line
