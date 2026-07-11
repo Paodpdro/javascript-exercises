82b8d88e6ffadb173eb538cb842fddeeb53735a5
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-07-12 00:09:04 +0300

    Finished 16th exercise - findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let result = people.reduce((acc, curr) => {
+            let currDeath = new Date().getFullYear();
+            let accDeath = new Date().getFullYear();
 
+            if (curr.yearOfDeath) {
+                currDeath = curr.yearOfDeath;
+            }
+
+            if (acc.yearOfDeath) {
+                accDeath = acc.yearOfDeath;
+            }
+
+            let currAge = currDeath - curr.yearOfBirth;
+            let accAge = accDeath - acc.yearOfBirth;
+            if (accAge < currAge) {
+                acc = curr;
+            }
+        return acc;
+    });
+    return result
 };
 
 // Do not edit below this line
