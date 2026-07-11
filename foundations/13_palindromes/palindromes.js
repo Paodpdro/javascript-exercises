ae9235a191b787541d06e1027cfebe3af5e76738
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-07-11 22:05:34 +0300

    Finished 13th exercise - palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const reversed = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, "").split('').reverse().join('')
+    const correct = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, "") 
+    return reversed == correct;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+    
